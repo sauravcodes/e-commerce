1fdf780180fd6e4e8682945f82bcd0481234b1c6
Saurav <saurav@getvital.in>
2021-02-23 21:09:00 +0530

    Added Logo Header component, shop and contact

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component.jsx";
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
